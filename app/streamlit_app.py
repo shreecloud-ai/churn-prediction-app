ab14aa808f171626d78750528a8022bf3fdbb816
unknown <shrgamer123@gmail.com>
2026-02-05 10:18:13 +0530

    Updated Streamlit to use live Render API URL

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -13,7 +13,7 @@ st.set_page_config(
 )
 
 # API endpoint (change when deployed)
-API_URL = "https://churn-api-toas.onrender.com"
+API_URL = "https://churn-api-toas.onrender.com/predict"
 # API_URL = "https://your-app-name.onrender.com/predict"  # after deployment
 
 # ────────────────────────────────────────────────
